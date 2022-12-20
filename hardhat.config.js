47c9a8774547109ead00ac5379c39b5a4c150776
Ahmed Zaher <contact@ahmedzaher.net>
2022-12-19 18:04:39 -0700

    Deployed Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
+    defaulNetwork: 'goerli',
+    networks:{
+      hardhat:{},
+      url: 'https://rpc.ankr.com/eth_goerli',
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
     settings: {
       optimizer: {
         enabled: true,
